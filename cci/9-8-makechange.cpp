21aa6d2044089504243bf5704d288c71f853cc2d
yang song <song@yangs-MacBook-Pro.local>
2014-07-26 20:49:10 -0400

    for 9.8, add function with memory/cache using map with pair as key

--- cci/9-8-makechange.cpp
+++ cci/9-8-makechange.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -32,6 +33,30 @@ int ways_makechanges(int N, int *coins, int index){
   return ways;
 }
 
+typedef pair<int, int> AmountIndex;
+
+int ways_makechanges_mem(int N, int *coins, int index, map<AmountIndex, int>& cache){
+  int ways = 0;
+  if(index >= 3) return 1;
+  AmountIndex current (N, index);
+  if(cache.find(current) != cache.end()){
+    // the ways for this amount is cached already
+    return cache[current];
+  }
+  
+  // not cached yet
+  int coin_value = *(coins+index);
+  int k = N/coin_value;
+  for(int i=0; i<=k; i++){
+    int RemainingCents = N - coin_value * i;
+      ways += ways_makechanges_mem(RemainingCents, coins, index+1, cache);
+  }
+  
+  cache.insert(std::make_pair(current, ways));
+  return ways;
+}
+
+
 void makechange(int n){
   int coins [] = {25, 10, 5, 1};
   int number = 0;
@@ -49,10 +74,34 @@ void makechange(int n){
   cout<<"There are "<<number<<" ways to make "<<n<<" cents using quarters, dimes, nickels and cents.\n";
 }
 
+void makechange_mem(int n){
+  int coins [] = {25, 10, 5, 1};
+  map<AmountIndex, int> cache;
+  int number = 0;
+  if(n>=25){
+    number = ways_makechanges_mem(n, coins, 0, cache);
+  }else if(n < 25 && n>= 10){
+    number = ways_makechanges_mem(n, coins, 1, cache);
+  }else if(n < 10 && n>= 5){
+    number = ways_makechanges_mem(n, coins, 2, cache);
+  }else if(n < 5 && n>=1){
+    number = ways_makechanges_mem(n, coins, 3, cache);
+  }else{
+    number = 0;
+  }
+  cout<<"There are "<<number<<" ways to make "<<n<<" cents using quarters, dimes, nickels and cents.\n";
+}
+
 int main(){
   makechange(1);
   makechange(5);
   makechange(10);
   makechange(25);
+  cout<<"------------------------------\n";
+
+  makechange_mem(1);
+  makechange_mem(5);
+  makechange_mem(10);
+  makechange_mem(25);
   return 0;
 }
