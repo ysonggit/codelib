44eee7b58da87fbcaaaf0d93339512f88e5bfb1f
Yang Song <ysong.sc@gmail.com>
2014-08-24 16:11:47 -0400

    solution 1.2 do not forget the specialcase checking

--- cci/1-2-reverse_string.cpp
+++ cci/1-2-reverse_string.cpp
@@ -13,6 +13,14 @@ void swapChars(char* a, char *b){
 // given a null-terminated string *char
 // return reversed string in form of *char
 void reverse(char *str){
+    // skip null pointer
+    if (str== 0){
+	return;
+    }
+    // skip empty string
+    if(*str == 0 ){
+	return;
+    }
     char* end = str+strlen(str) - 1; // the last non-null character
     while(str < end){
 	swapChars(str, end);
@@ -32,5 +40,8 @@ int main(){
     char a1[]= "this is a message";
     reverse(a1);
     printChars(a1);
+    char a2[] ="";
+    reverse (a2);
+    printChars(a2);
     return 0;
 }
