aa21cd8e101e1529d98d9c22e5ffb597a9375afa
Yang Song <ysong.sc@gmail.com>
2014-09-08 23:46:41 -0400

    start working on matrix rotation problem

---
+++ cci/1-6-rotate_matrix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+  cci solution 1.6
+  given an image represented by NxN matrix, each pixel is 4 byte (32bit).
+  write a method to rotate the matrix by 90 degrees, do it in place.
+ */
+
+int main(){
+
+
+    return 0;
+}
