d09a1ba08167d29a81d9b6dd4a05f44ca2c237bc
Yang Song <ysong.sc@gmail.com>
2014-09-14 00:19:06 -0400

    figured out the shared_ptr usage

--- prac/test_shared_ptr.cpp
+++ prac/test_shared_ptr.cpp
@@ -38,6 +38,8 @@ public:
     shared_ptr<MailBox> ptr_mbox;
     Mail createMail(string, string);
     void dropMail(Mail, MailBox&);
+    void openMail(int);
+    void openMail(int, MailBox &);
 };
 
 Member::Member(int i, string s, shared_ptr<MailBox> p){
@@ -58,6 +60,20 @@ void Member::dropMail(Mail m, MailBox & mb){
     mb.mails[uid] = m;
 }
 
+// open user X 's mail in the mailbox
+void Member::openMail(int x){
+    Mail m = (*ptr_mbox.get()).mails[x];
+    cout<<"User "<<name<<" opens mail from "<<m.sender<<endl;
+    cout<<m<<endl;
+}
+
+void Member::openMail(int x, MailBox & mb){
+    Mail m = mb.mails[x];
+    cout<<"User "<<name<<" opens mail from "<<m.sender<<endl;
+    cout<<m<<endl;
+}
+
+
 // A Group owns serveral members and a post box
 // every member can drop his/her mail to the Mail box
 class Group{
@@ -70,7 +86,7 @@ public:
     MailBox mbox;
     void showMails(){
 	for(auto it = mbox.mails.begin(); it != mbox.mails.end(); ++it){
-	    cout<<it->first<<endl;
+	    cout<<"Mail ("<< it->first<<")"<<endl;
 	    cout<<it->second<<endl;
 	    cout<<"======================================="<<endl;
 	}
@@ -81,6 +97,7 @@ public:
 Group::Group(int n) : number_users(n){
     mbox.date = 1;
     shared_ptr<MailBox> _ptr_mbox = make_shared<MailBox>(mbox);
+    //cout<<(*_ptr_mbox).date<<endl;
     for(int i=0; i<n; i++){
 	// convert int ascii to char
 	char c = i+65;
@@ -101,5 +118,10 @@ int main(){
     Mail m1 = user1.createMail("Mike", "hello");
     user1.dropMail(m1, A.mbox);
     A.showMails();
+    Member user5 = A.users.find(5)->second;
+    user5.ptr_mbox = make_shared<MailBox>(A.mbox);
+    user5.openMail(1);
+    //user5.openMail(1, A.mbox); 
+    
     return 0;
 }
