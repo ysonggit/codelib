8c354d4f6369cf14b2895c8432aec68890ffbc76
Yang Song <ysong.sc@gmail.com>
2014-10-17 20:05:22 -0400

    cast return type of std::distance to int in test_boostfilesys.cpp

--- prac/test_boostfilesys.cpp
+++ prac/test_boostfilesys.cpp
@@ -8,15 +8,14 @@ namespace fs = boost::filesystem;
 // g++ -std=c++0x test_boostfilesys.cpp -lboost_system -lboost_filesystem
 
 int main(){
-    fs::path p("./cache/");
+    fs::path p("./");
     fs::directory_iterator dir_first(p), dir_last;
 
     auto pred = [](const fs::directory_entry& p){
         return fs::is_regular_file(p);
     };
-
-    std::cout <<
-        std::distance(boost::make_filter_iterator(pred, dir_first, dir_last),
-                      boost::make_filter_iterator(pred, dir_last, dir_last))<<std::endl;
+    int num =  std::distance(boost::make_filter_iterator(pred, dir_first, dir_last),
+			     boost::make_filter_iterator(pred, dir_last, dir_last));
+    std::cout <<num <<std::endl;
     return 0;
 }
