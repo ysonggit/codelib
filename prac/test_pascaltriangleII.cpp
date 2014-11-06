dbdbe335cebaec80f0813969aa6b05f947b7e9ee
Yang Song <ysong.sc@gmail.com>
2014-11-06 17:02:47 -0500

    update pascal's triangle solution 2

--- prac/test_pascaltriangleII.cpp
+++ prac/test_pascaltriangleII.cpp
@@ -32,11 +32,35 @@ vector<int> getRow(int rowIndex) {
     return row;
 }
 
+vector<int> getRow2(int rowIndex){
+    vector<int> row;
+    if(rowIndex<0) return row;
+    row.resize(rowIndex+1);
+    row[0] = 1;
+    if(rowIndex == 0) return row; 
+    for(int r = 1; r<=rowIndex; r++){
+	for(int i=rowIndex; i>=0; i--){
+	    if(i==0 || i==rowIndex){
+		row[i] = 1;
+	    }else{
+		row[i] = row[i]+ row[i-1];
+	    }
+	}
+    }
+    return row;
+}
 
 int main(){
     vector<int> row = getRow(4);
     cout<<"----result------\n";
     for(int i : row){
+	cout<<i<<" ";
+    }
+    cout<<endl;
+
+    vector<int> row2 = getRow2(4);
+    cout<<"----result 2------\n";
+    for(int i : row2){
 	cout<<i<<" ";
     }
     cout<<endl;
