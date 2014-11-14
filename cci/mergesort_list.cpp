6413eb793053d642615899277a28a86681553ecc
Yang Song <ysong.sc@gmail.com>
2014-11-14 03:01:55 -0500

    add other's work that passed the online judge

--- cci/mergesort_list.cpp
+++ cci/mergesort_list.cpp
@@ -72,12 +72,76 @@ ListNode * merge(ListNode * head){
     return mergeSort(merge(head), merge(second));
 }
 
-// merge sort list O(n log n) using O(1) space 
+// merge sort list O(n log n) using O(1) space leetcode version
+/*
 ListNode *sortList(ListNode *head) {  
     if(!head || !head->next) return head;
     return  merge(head);
 }
-
+*/
+ListNode *sortList(ListNode *head) {  
+    // Get length first  
+    ListNode* p = head;  
+    int len = 0;  
+    while (p != NULL)  
+    {  
+	p = p->next;  
+	len++;  
+    }  
+    ListNode* fakehead = new ListNode(-1);  
+    fakehead->next = head;       
+    for (int interval = 1; interval <= len; interval = interval * 2)  
+    {  
+	ListNode* pre = fakehead;  
+	ListNode* slow = fakehead->next, *fast = fakehead->next;  
+	while (fast != NULL || slow != NULL)  
+	{  
+	    int i = 0;  
+	    while (i< interval && fast != NULL)  
+	    {  
+		fast = fast->next; //move fast pointer ahead 'interval' steps  
+		i++;  
+	    }  
+	    //merge two lists, each has 'interval' length  
+	    int fvisit = 0, svisit = 0;  
+	    while (fvisit < interval && svisit<interval && fast != NULL && slow != NULL) {  
+		if (fast->data < slow->data)  
+		{  
+		    pre->next = fast;  
+		    pre = fast;  
+		    fast = fast->next;  
+		    fvisit++;  
+		}  
+		else  
+		{  
+		    pre->next = slow;  
+		    pre = slow;  
+		    slow = slow->next;  
+		    svisit++;  
+		}  
+	    }  
+	    while (fvisit < interval && fast != NULL)  
+	    {  
+		pre->next = fast;  
+		pre = fast;  
+		fast = fast->next;  
+		fvisit++;  
+	    }  
+	    while (svisit < interval && slow != NULL)  
+	    {  
+		pre->next = slow;  
+		pre = slow;  
+		slow = slow->next;  
+		svisit++;  
+	    }  
+	    pre->next = fast;  
+	    slow = fast;  
+	}  
+    }  
+    ListNode* newhead = fakehead->next;  
+    delete fakehead;  
+    return newhead;  
+}  
 
 int main(){
     ListNode * head = new ListNode(4);
